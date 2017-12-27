eb37a5f2f3d322f13f4e03f47acf540c80cccd7b
Dharshana Ratnayake <302701@twgroup.co.nz>
2017-12-27 13:35:51 +1300

    add Jenkins file

---
+++ build/pipline_job.groovy
@@ -0,0 +1,16 @@
+pipelineJob('jenkins-sample-application-build') {
+    description('Build Jenkins Sample Application')
+    triggers {
+        scm('*/5 * * * *')
+    }
+    definition {
+        cpsScm {
+            scm {
+                git('git@github.com:darthShana/jenkins-sample-application.git') {
+                    node -> node / extensions()
+                }
+            }
+            scriptPath('build/Jenkinsfile')
+        }
+    }
+}
